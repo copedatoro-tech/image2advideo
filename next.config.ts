b2a0d74e503664166cfb368e445fd7e355c4df3f
Dan Cozma <copedatoro@gmail.com>
2026-01-28 17:12:25 +0200

    Fix config and move success page to root

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   output: 'standalone',
-  // 👇 redirect explicit pentru /success
-  async redirects() {
-    return [
-      {
-        source: '/success',
-        destination: '/(client)/success',
-        permanent: false,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
