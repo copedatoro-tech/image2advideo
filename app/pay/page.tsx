302a80d7b3ece5b808b918fb564442b1e7ebaf71
copedatoro-tech <copedatoro@gmail.com>
2026-02-26 19:19:22 +0000

    Fix pay page: wrap in Suspense

--- app/pay/page.tsx
+++ app/pay/page.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import { Suspense } from "react";
 import PayClient from "./PayClient";
 
-export default function PayPage({ searchParams }: any) {
-  return <PayClient />;
+export default function PayPage() {
+  return (
+    <Suspense fallback={<p>Se încarcă...</p>}>
+      <PayClient />
+    </Suspense>
+  );
 }
 
